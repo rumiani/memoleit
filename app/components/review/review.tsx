c40bc8c35be4c4f91cac5a4e5e95c38a1d60ba77
rumiani <maziar.rumiani@gmail.com>
2024-03-18 02:29:02 +0330

    seperated development and production items

--- app/components/review/review.tsx
+++ app/components/review/review.tsx
@@ -1,13 +1,20 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import words from '@/data/11plus.json'
-// import words from "@/data/4.json";
+import words_11plus  from "@/data/11plus.json";
+import words_test from "@/data/4.json";
 import storeToLocal from "./storeToLocal";
 import { item } from "@/types/interface";
 import { reviewHandler } from "@/handlers/reviewHandler";
 import { randomItemHandler } from "@/handlers/randomItemHandler";
 import { learnedHandler } from "@/handlers/learnedHandler";
 import Item from "./item/item";
+console.log(process.env.NODE_ENV);
+let words: string[];
+if (process.env.NODE_ENV === "development") {
+  words = words_test;
+} else {
+  words = words_11plus;
+}
 
 export default function Review() {
   const [item, setItem] = useState<item>();
@@ -23,18 +30,18 @@ export default function Review() {
     }
   }, []);
 
-  const goToNextItem = (item: item, status: boolean) => {    
+  const goToNextItem = (item: item, status: boolean) => {
     const newRandomItem = learnedHandler(item, status);
     setItem(newRandomItem);
   };
 
   return (
     <div className="word-box border border-gray-300 rounded-lg p-6 flex flex-col justify-between w-80 mx-auto mt-10">
-      {item?
-    <Item item={item} days={days} goToNextItem={goToNextItem} />
-    :
-    <p className="text-red-500">There is no item to review.</p>  
-    }
+      {item ? (
+        <Item item={item} days={days} goToNextItem={goToNextItem} />
+      ) : (
+        <p className="text-red-500">There is no item to review.</p>
+      )}
     </div>
   );
 }
