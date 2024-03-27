3effda83b8d83294df02e45559ce8ea516f55fd5
rumiani <maziar.rumiani@gmail.com>
2024-03-27 18:45:37 +0330

    input filter check style fixed

--- src/components/home-page-components/navItems/filters/form/input/input.tsx
+++ src/components/home-page-components/navItems/filters/form/input/input.tsx
@@ -17,7 +17,7 @@ export default function CheckboxInput({
     unLearned: number;
   }>({ all: 0, learned: 0, unLearned: 0 });
 
-  const inputChangeHandler = (e:FormEvent) => {
+  const inputChangeHandler = (e: FormEvent) => {
     setIsChecked(!isChecked);
     handleInputChange(e);
   };
@@ -29,40 +29,40 @@ export default function CheckboxInput({
 
   return (
     <div>
-      <div className="flex">
-      <input
-        checked={isChecked}
-        onChange={(e) => inputChangeHandler(e)}
-        type="checkbox"
-        id={catagory}
-        name={catagory}
-        className=" cursor-pointer
+      <div className="flex relative">
+        <input
+          checked={isChecked}
+          onChange={(e) => inputChangeHandler(e)}
+          type="checkbox"
+          id={catagory}
+          name={catagory}
+          className="cursor-pointer
 relative peer shrink-0 flex justify-center align-middle
 appearance-none w-5 h-5 border-2 border-blue-500 rounded-sm bg-white
 mt-1 mx-0
 checked:bg-blue-600 checked:border-0"
-      />
-      <label htmlFor={catagory} className="cursor-pointer px-2">
-        {catagory}
-      </label>
-      <svg
-        className="
+        />
+        <label htmlFor={catagory} className="cursor-pointer px-2">
+          {catagory}
+        </label>
+        <svg
+          className="
         cursor-pointer
 absolute 
 w-4 h-4 mt-[6px] ml-[2px]
 hidden peer-checked:block
 pointer-events-none"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="white"
-        strokeWidth="4"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      >
-        <polyline points="20 6 9 17 4 12"></polyline>
-      </svg>
-    </div>
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="white"
+          strokeWidth="4"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <polyline points="20 6 9 17 4 12"></polyline>
+        </svg>
+      </div>
       <div className="pr-4 w-full text-left flex justify-between">
         <span>All:{itemInfo.all}</span>
         <span className="text-green-600">Learned:{itemInfo.learned}</span>
