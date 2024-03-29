8a3d2532dad8b6369b04fc3bd4f14ed04c0a1e1a
rumiani <maziar.rumiani@gmail.com>
2024-03-29 20:50:48 +0330

    fixing the navitems style

--- src/components/home-page-components/navItems/NavItems.tsx
+++ src/components/home-page-components/navItems/NavItems.tsx
@@ -4,20 +4,19 @@ import React from "react";
 import { FaChartBar } from "react-icons/fa";
 import { MdNoteAdd } from "react-icons/md";
 import Link from "next/link";
-import Filters from "./filters/filters";
-
+import { MdFilterListAlt } from "react-icons/md";
 export default function ItemsNav() {
   return (
     <div className="flex w-full pb-4 border-b border-gray-300">
-      <Link href={"/progress"} title="Check out your progress">
-        <FaChartBar className="text-blue-700 icon" />
+      <Link href={"/progress"} title="Check out your progress" className="icon">
+        <FaChartBar className="text-blue-700 text-xl" />
       </Link>
 
-      <span className="icon" title="Filter catagories">
-      <Filters/>
+      <span className="icon text-3xl" title="Filter catagories">
+      <MdFilterListAlt/>
       </span>
 
-      <Link href={"/new"} >
+      <Link href={"/new"} className="icon">
         <MdNoteAdd title="Add a new item" className="text-green-700 icon" />
       </Link>
     </div>
