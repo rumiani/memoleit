966666a38c1e0aca1bd3369e764676b29a718807
rumiani <maziar.rumiani@gmail.com>
2024-04-13 15:14:36 +0330

    show description bug fixed

--- src/components/dashboard-page/review/reviewItem/itemBody/itemBody.tsx
+++ src/components/dashboard-page/review/reviewItem/itemBody/itemBody.tsx
@@ -1,11 +1,17 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function ItemBody({ body }: { body: string }) {
   const [showBody, setShowBody] = useState<boolean>(false);
-
+  useEffect(() => {
+    setShowBody(false);
+  }, []);
   return (
     <div>
-      {showBody && <p className="text-gray-600">{body.length === 0? 'There is no description for this item.':body}</p>}
+      {showBody && (
+        <p className="text-gray-600">
+          {body.length === 0 ? "There is no description for this item." : body}
+        </p>
+      )}
       <button
         onClick={() => setShowBody(!showBody)}
         className="text-blue-500 w-fit mx-auto block text-center"
