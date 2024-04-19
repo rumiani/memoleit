30960eb8a783eba07510c58addd3c4e79818118a
rumiani <maziar.rumiani@gmail.com>
2024-04-19 13:58:47 +0330

    total data component in progress page improved

--- src/components/progress-page/boxChart/totalData/totalData.tsx
+++ src/components/progress-page/boxChart/totalData/totalData.tsx
@@ -1,26 +1,57 @@
 import { categoryItemsCountHandler } from "@/src/handlers/categoryItemsCountHandler";
-import React from "react";
+import React, { useEffect, useState } from "react";
 interface DataType {
   name: string;
   Reviewed: number;
   Pending: number;
 }
-export default function TotalData({ data,category }: { data: DataType[] | undefined,category:string }) {
-  let reviewedNumber = 0;
-  let pendingNumber = 0;
-  const categoryInfo = categoryItemsCountHandler(category)
-  if (data) {
-    data.forEach((box: any) => {
-      reviewedNumber += box.Reviewed;
-      pendingNumber += box.Pending;
-    });
-  }
+export default function TotalData({
+  data,
+  category,
+}: {
+  data: DataType[] | undefined;
+  category: string;
+}) {
+  const [reviewedNumber, setReviewedNumber] = useState<number>(0);
+  const [pendingNumber, setPendingNumber] = useState<number>(0);
+  const [categoryInfo, setCategoryInfo] = useState<{
+    allItemsCount: number;
+    learnedCount: number;
+    unLearnedCount: number;
+  } | null>(null);
+
+  useEffect(() => {
+    if (data) {
+      let reviewed = 0;
+      let pending = 0;
+      data.forEach((box: any) => {
+        reviewed += box.Reviewed;
+        pending += box.Pending;
+      });
+      setReviewedNumber(reviewed);
+      setPendingNumber(pending);
+    }
+
+    const categoryData = categoryItemsCountHandler(category);
+    if (categoryData) {
+      setCategoryInfo(categoryData);
+    }
+  }, [data, category]);
+
   return (
     <div className=" flex flex-wrap gap-1 w-full justify-start cursor-default">
-      <span className="text-blue-600 w-44 h-6 ">All The Items: {categoryInfo?.allItemsCount}</span>
-      <span className="text-green-800 w-44 h-6">Learned Items: {categoryInfo?.learnedCount}</span>
-      <span className="text-orange-500 w-44 h-6">Items In The Box: {categoryInfo?.unLearnedCount}</span>
-      <span className="text-green-500 w-44 h-6">Reviewed: {reviewedNumber}</span>
+      <span className="text-blue-600 w-44 h-6 ">
+        All The Items: {categoryInfo?.allItemsCount}
+      </span>
+      <span className="text-green-800 w-44 h-6">
+        Learned Items: {categoryInfo?.learnedCount}
+      </span>
+      <span className="text-orange-500 w-44 h-6">
+        Items In The Box: {categoryInfo?.unLearnedCount}
+      </span>
+      <span className="text-green-500 w-44 h-6">
+        Reviewed: {reviewedNumber}
+      </span>
       <span className="text-red-500 w-44 h-6">Pending: {pendingNumber}</span>
     </div>
   );
