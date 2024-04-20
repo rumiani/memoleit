62622cd7ee27df3ca8e16bcaac566159bc5f8a18
rumiani <maziar.rumiani@gmail.com>
2024-04-20 10:00:44 +0330

    body no value for item in category fixed

--- src/components/category-page/categoryItems/categoryItem/categoryitem/itemBody/itemBody.tsx
+++ src/components/category-page/categoryItems/categoryItem/categoryitem/itemBody/itemBody.tsx
@@ -1,12 +1,18 @@
 import { convertFromRaw } from "draft-js";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function ItemBody({ body }: { body: string }) {
   const [showBody, setShowBody] = useState<boolean>(false);
+  const [bodyValue, setBodyValue] = useState<string>('');
 
+  useEffect(() => {
+    setShowBody(false);
+    const convertedFromRaw = convertFromRaw(JSON.parse(body)).getPlainText()
+    setBodyValue(convertedFromRaw)
+  }, [body]);
   return (
     <div>
-      {showBody && body && <p className="text-gray-600">{convertFromRaw(JSON.parse(body)).getPlainText()}</p>}
+      {showBody && body && <p className="text-gray-600">{bodyValue}</p>}
       <button
         onClick={() => setShowBody(!showBody)}
         className="text-blue-500 w-fit mx-auto block text-center"
--- src/components/dashboard-page/review/reviewItem/itemBody/itemBody.tsx
+++ src/components/dashboard-page/review/reviewItem/itemBody/itemBody.tsx
@@ -3,16 +3,20 @@ import React, { useEffect, useState } from "react";
 
 export default function ItemBody({ body }: { body: string }) {
   const [showBody, setShowBody] = useState<boolean>(false);
+  const [bodyValue, setBodyValue] = useState<string>('');
+
 
   useEffect(() => {
     setShowBody(false);
+    const convertedFromRaw = convertFromRaw(JSON.parse(body)).getPlainText()
+    setBodyValue(convertedFromRaw)
   }, [body]);
 
   return (
     <div className="mt-4">
       {showBody && (
         <p className="text-gray-600">
-          {body.length === 0 ? "There is no description for this item." : convertFromRaw(JSON.parse(body)).getPlainText()}
+          {body.length === 0 ? "There is no description for this item." : bodyValue}
         </p>
       )}
       <button
