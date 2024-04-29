db4821ed9a937132ce0b266d9601217f5d56cc2c
maziar <maziar.rumiani@gmail.com>
2024-04-30 03:17:32 +0330

    category items filter fixed

--- src/handlers/categoryItemsCountHandler.ts
+++ src/handlers/categoryItemsCountHandler.ts
@@ -1,18 +1,22 @@
 import { itemTypes } from "../types/interface";
 import { getAppDataHandler } from "./getAppDataHandler";
+import { makeUrlFriendly } from "./makeUrlFriendly";
 
-export const categoryItemsCountHandler = (topic: string) => {
+export const categoryItemsCountHandler = (category: string) => {
   if (typeof window !== "undefined") {
     const allItems = getAppDataHandler().itemsData;
-    const categoryItems: itemTypes[] = allItems.filter(
-      (item: itemTypes) => item.category === topic
-    );
+    const categoryItems: itemTypes[] = allItems.filter((item: itemTypes) => {
+      return (
+        makeUrlFriendly(item.category).toLowerCase() ===
+        makeUrlFriendly(category).toLowerCase()
+      );
+    });
 
     let allItemsCount: number;
     let learnedCount: number;
     let unLearnedCount: number;
 
-    if (topic === "") {
+    if (category === "") {
       const allLearnedItems = allItems.filter(
         (item: itemTypes) => item.reviews.box > 5
       );
