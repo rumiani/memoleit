483b02fcf9666ae5a71cbe000855f43d02fe6ccf
maziar <maziar.rumiani@gmail.com>
2024-06-15 12:49:26 +0330

    text hilight tooltip for mobile added

--- src/components/dashboard-super-page/docs-page/book-page/bookPage.tsx
+++ src/components/dashboard-super-page/docs-page/book-page/bookPage.tsx
@@ -82,6 +82,7 @@ export default function BookPage({ id }: { id: string }) {
               file={pdfUrl}
               onLoadSuccess={onDocumentLoadSuccess}
               onMouseUp={handleTextHighlight}
+              onTouchStart={handleTextHighlight}
             >
               {Array.from(new Array(numPages), (el, index) => (
                 <Page
