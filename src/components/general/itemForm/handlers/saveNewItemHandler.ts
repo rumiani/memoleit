8f9e20db2efcf9cdad2c34ff96b2eff7cefb4dee
maziar <maziar.rumiani@gmail.com>
2024-06-16 22:57:53 +0330

    new item category bug fixed

--- src/components/general/itemForm/handlers/saveNewItemHandler.ts
+++ src/components/general/itemForm/handlers/saveNewItemHandler.ts
@@ -6,59 +6,48 @@ import { randomIdGenerator } from "@/src/handlers/randomID";
 import { FormValues, ItemTypes, CategoryTypes } from "@/src/types/interface";
 import { makeUrlFriendly } from "@/src/handlers/makeUrlFriendly";
 
-export const saveNewItemToLocal = ({ title, body, category }: FormValues) => {
-  db.categories
-    .where("name")
-    .equals(makeUrlFriendly(category))
-    .first()
-    .then((storedCategory) => {
-      if (storedCategory) {
-        createNewItemHandler(storedCategory.id);
-      } else {
-        createNewCategoryHandler(category);
-      }
-    })
-    .catch((error) => {
-      console.log("Error retrieving category:", error);
-    });
-
-  const createNewItemHandler = (categoryId: string) => {
-    const itemObject: ItemTypes = {
-      id: uuidv4(),
-      userId: userIdTest,
-      categoryId,
-      title:makeUrlFriendly(title),
-      body,
-      category:makeUrlFriendly(category),
-      box: 1,
-      createdAt: Date.now(),
-      lastReview: Date.now(),
+export const saveNewItemToLocal = async ({
+  title,
+  body,
+  category,
+}: FormValues) => {
+  try {
+    const createNewItemHandler = async (categoryId: string) => {
+      const itemObject: ItemTypes = {
+        id: uuidv4(),
+        userId: userIdTest,
+        categoryId,
+        title: makeUrlFriendly(title),
+        body,
+        category: makeUrlFriendly(category),
+        box: 1,
+        createdAt: Date.now(),
+        lastReview: Date.now(),
+      };
+      await db.items.add(itemObject);
+      toast.success("The new item has been saved.");
     };
-    db.items
-      .add(itemObject)
-      .then((id) => {
-        toast.success("The new item has been saved.");
-      })
-      .catch((error) => {
-        console.log(`error adding item`, error);
-      });
-  };
 
-  const createNewCategoryHandler = (category: string) => {
-    const categoryObject: CategoryTypes = {
-      id: randomIdGenerator(8),
-      userId: userIdTest,
-      name: makeUrlFriendly(category),
-      status: 0,
-      createdAt: Date.now(),
+    const createNewCategoryHandler = async (category: string) => {
+      const categoryObject: CategoryTypes = {
+        id: randomIdGenerator(8),
+        userId: userIdTest,
+        name: makeUrlFriendly(category),
+        status: 0,
+        createdAt: Date.now(),
+      };
+      const savedCategoryId = await db.categories.add(categoryObject);
+      createNewItemHandler(savedCategoryId);
     };
-    db.categories
-      .add(categoryObject)
-      .then((id) => {
-        createNewItemHandler(id);
-      })
-      .catch((error) => {
-        console.log(`error adding item`, error);
-      });
-  };
+
+    const foundCategory = await db.categories
+      .where("name")
+      .equals(makeUrlFriendly(category))
+      .first();
+    if (foundCategory) {
+      createNewItemHandler(foundCategory.id);
+    } else {
+      createNewCategoryHandler(category);
+    }
+  } catch (error) {}
 };
