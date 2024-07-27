eeec30d632766c768058acca5cf593ec37d1ea1a
maziar <maziar.rumiani@gmail.com>
2024-07-27 17:40:58 +0330

    suspence added for useSearchParams

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,29 +1,29 @@
-// // pages/api/search.ts
-// import { db } from "@/src/services/db";
-// import type { NextApiRequest, NextApiResponse } from "next";
-// import { NextResponse } from "next/server";
+// pages/api/search.ts
+import { db } from "@/src/services/db";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { NextRequest, NextResponse } from "next/server";
 
-// export async function GET(req: NextApiRequest, res: NextApiResponse) {
-//   const url = new URL(req.url!);
-//   const query = url.searchParams.get("q") || "";
+export async function GET(req: NextRequest, res: NextApiResponse) {
+  const url = new URL(req.url!);
+  const query = url.searchParams.get("q") || "";
 
-//   if (!query) {
-//     return NextResponse.json(
-//       { error: "Query parameter is required" },
-//       { status: 400 },
-//     );
-//   }
+  if (!query) {
+    return NextResponse.json(
+      { error: "Query parameter is required" },
+      { status: 400 },
+    );
+  }
   
-//   try {
+  try {
 
 
-//     return NextResponse.json('Hellow');
-//   } catch (error) {
-//     console.log(error);
+    return NextResponse.json('Hellow');
+  } catch (error) {
+    console.log(error);
     
-//     return NextResponse.json(
-//       { error: "Failed to fetch data" },
-//       { status: 500 },
-//     );
-//   }
-// }
+    return NextResponse.json(
+      { error: "Failed to fetch data" },
+      { status: 500 },
+    );
+  }
+}
--- src/app/user/(userLayout)/box/(boxLayout)/search/page.tsx
+++ src/app/user/(userLayout)/box/(boxLayout)/search/page.tsx
@@ -1,11 +1,13 @@
 "use client";
-import React from "react";
+import React, { Suspense } from "react";
 import SearchPage from "@/src/components/user-pages/box/search-page/searchPage";
 
 export default function Search() {
   return (
     <div>
-      <SearchPage />
+      <Suspense>
+        <SearchPage />
+      </Suspense>
     </div>
   );
 }
