ccea9a7c78183c1e555704500f314652e91c3757
maziar <maziar.rumiani@gmail.com>
2024-07-31 17:20:53 +0330

    fixed the loadingPulse style

--- src/components/general/loading-comps/loadingPulse/loadingPulse.tsx
+++ src/components/general/loading-comps/loadingPulse/loadingPulse.tsx
@@ -4,7 +4,6 @@ export default function LoadingPulse() {
   return (
     <div className="animate-pulse border-opacity-30 border border-slate-400 shadow rounded-md p-4 w-full sm:w-72  mx-auto">
       <div className=" flex space-x-4">
-        <div className="rounded-full bg-slate-400 h-10 w-10"></div>
         <div className="flex-1 space-y-6 py-1">
           <div className="h-2 bg-slate-400 rounded"></div>
           <div className="space-y-3">
