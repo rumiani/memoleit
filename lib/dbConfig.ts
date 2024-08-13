24871f4649a9a71fb034f64b9ae0be1ebae092d2
maziar <maziar.rumiani@gmail.com>
2024-08-13 22:26:41 +0330

    working on the github auth

--- lib/dbConfig.ts
+++ lib/dbConfig.ts
@@ -1,20 +1,20 @@
 import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI!;
-
-if (!MONGODB_URI) {
-  throw new Error("MONGODB_URI must be defined");
-}
-
+if (!MONGODB_URI) throw new Error("MONGODB_URI must be defined");
+const opts = {
+  bufferCommands: false,
+  serverSelectionTimeoutMS: 5000, // Increase the timeout to 5000ms (5 seconds)
+};
 export const connectDB = async () => {
   try {
-    const { connection } = await mongoose.connect(MONGODB_URI);
+    const { connection } = await mongoose.connect(MONGODB_URI, opts);
     if (connection.readyState === 1) {
-      console.log("MongoDB Connected");
+      console.log("10:MongoDB Connected");
       return Promise.resolve(true);
     }
   } catch (error) {
-    console.error("-------",error);
+    console.error("-------", error);
     return Promise.reject(error);
   }
 };
