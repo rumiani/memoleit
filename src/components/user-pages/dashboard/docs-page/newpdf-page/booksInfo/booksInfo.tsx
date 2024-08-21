8f200d9f2cc6a0004be7da6f877a46a237d4f9fd
maziar <maziar.rumiani@gmail.com>
2024-08-21 15:47:46 +0330

    minor change

--- src/components/user-pages/dashboard/docs-page/newpdf-page/booksInfo/booksInfo.tsx
+++ src/components/user-pages/dashboard/docs-page/newpdf-page/booksInfo/booksInfo.tsx
@@ -2,7 +2,8 @@ import Dialog from "@/src/components/general/dialog/dialog";
 import LeitnerPic from "@/src/components/general/item/itemInfo/leitnerPic";
 import React, { useState } from "react";
 import { IoInformationCircleOutline } from "react-icons/io5";
-
+import { Tooltip as ReactTooltip } from "react-tooltip";
+import { IoMdCloseCircle } from "react-icons/io";
 export default function BooksInfo() {
   const [isDialogOpen, setDialogOpen] = useState(false);
   const openDialog = () => setDialogOpen(true);
@@ -19,12 +20,17 @@ export default function BooksInfo() {
       </button>
 
       <Dialog isOpen={isDialogOpen} onClose={closeDialog}>
-        <div className="flex flex-col items-start">
-          Add PDF books to the app and study.
-          <br />
-          Highlight new words to check their meaning and pronunciation, then add
-          them to the Leitner box for later review, all without interrupting your
-          reading.
+        <div className="relative pt-4">
+          <IoMdCloseCircle
+          onClick={closeDialog}
+          className="absolute right-0 top-0 text-2xl cursor-pointer text-red-500" />
+          <div className="flex flex-col items-start">
+            Add PDF books to the app and study.
+            <br />
+            Highlight new words to check their meaning and pronunciation, then
+            add them to the Leitner box for later review, all without
+            interrupting your reading.
+          </div>
         </div>
       </Dialog>
     </div>
