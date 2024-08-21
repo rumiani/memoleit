a8aec5e81a584e1617bbde02595cf10c5df17e18
maziar <maziar.rumiani@gmail.com>
2024-08-21 13:44:07 +0330

    appDataInitialiser and setting loading added to user layout

--- src/app/user/(userLayout)/dashboard/(dashboardLayout)/layout.tsx
+++ src/app/user/(userLayout)/dashboard/(dashboardLayout)/layout.tsx
@@ -1,7 +1,26 @@
 "use client";
+import { useAppDispatch } from "@/src/app/hooks";
 import { superPages } from "@/src/components/general/layouts/generalLayout/header/profile/userOpetions/superpages/superpages";
 import SuperPageLayout from "@/src/components/general/layouts/superPageLayout/superPageLayout";
+import { appDataInitialiser } from "@/src/handlers/appDataInitialiser";
+import { storedSettingReducer } from "@/src/redux/slices/settingStateSlice";
+import { db } from "@/src/services/db";
+import { useEffect } from "react";
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    appDataInitialiser();
+    db.setting
+      .where("name")
+      .equals("setting")
+      .first()
+      .then((storedSetting) => {
+        if (storedSetting) dispatch(storedSettingReducer(storedSetting!));
+      })
+      .catch(() => {
+        console.log("Error");
+      });
+  }, [dispatch]);
   return (
     <section>
       <nav>
--- src/components/user-pages/dashboard/review-page/ReviewPage.tsx
+++ src/components/user-pages/dashboard/review-page/ReviewPage.tsx
@@ -22,6 +22,8 @@ export default function ReviewPage() {
     const fetchItems = async () => {
       try {
         const numberOfItems = await numberOfItemsToReviewHandler();
+        console.log(numberOfItems);
+        
         if (numberOfItems)
           dispatch(numberOfItemsToReviewReducer(numberOfItems));
         const itemsToReview = await itemsToReviewWithActiveCategoryHandler();
