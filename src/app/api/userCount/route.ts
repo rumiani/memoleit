5e8ea10b91d3b1661b9972989ec2ba46c4d66563
maziar <maziar.rumiani@gmail.com>
2024-08-31 17:40:02 +0330

    authOptions moved to lib folder

--- src/app/api/userCount/route.ts
+++ src/app/api/userCount/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { connectDB } from "@/lib/dbConfig";
 import User from "@/utils/models/user";
 import { getServerSession } from 'next-auth';
-import { authOptions } from "../auth/[...nextauth]/route";
+import { authOptions } from "@/lib/auth/authOptions";
 
 export async function GET(req: NextRequest) {
   const session = await getServerSession(authOptions);  
